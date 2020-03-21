a209d7ef063a964090ec1abdda6423e954c90635
Janina Zylinska <janeczka@gmail.com>
2020-03-21 16:51:46 +0000

    Add routing for Home and About page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// step_03 add both components for setting up routs
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // step_04 add routes directing and redirecting to components
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
