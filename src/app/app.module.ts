34923aa292477450d3de129dfdb6fbc61257bc98
Janina Zylinska <janeczka@gmail.com>
2020-03-21 15:54:06 +0000

    add httpClient to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// step_01 add HttpClientModule to your project
+// now we can use HttpClient service in our Angular project to consume our REST API
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
