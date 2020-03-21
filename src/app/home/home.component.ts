e558fcf89b273bce544d7f1da603cd06534a3ec4
Janina Zylinska <janeczka@gmail.com>
2020-03-21 20:43:27 +0000

    Add get method of apiService to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+// we import ApiService defined in previous step that contains get() method
+// which fetches data form json rest api server
+import { ApiService } from '../api.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.get().subscribe((data: any[]) => { //calls the get() method of the service for fetching data from the JSON REST API server
+			console.log(data);
+			this.products = data;
+		})
   }
-
 }
